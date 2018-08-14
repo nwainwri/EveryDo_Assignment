a5b0aa3d0824c664a5d73f6a3c83ab016b580ef4
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-14 13:49:19 -0700

    Initial Commit

---
+++ EveryDo_Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EveryDo_Assignment
+//
+//  Created by Nathan Wainwright on 2018-08-14.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EveryDo_Assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EveryDo_Assignment
+//
+//  Created by Nathan Wainwright on 2018-08-14.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
